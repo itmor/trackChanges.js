09cc3e76c7bac460104e5c8cc33faa089b88f914
itmor <itmor.developer@gmail.com>
2020-05-11 17:21:05 +0300

    add TrackChanges class

--- src/trackChanges.js
+++ src/trackChanges.js
@@ -1,1 +1,24 @@
-// clean
+class TrackChanges {
+  state = {
+    workerActive: false,
+  };
+
+  settings = {
+    workerCallInterval: null,
+  };
+
+  constructor(workerCallInterval = 100) {
+    if (typeof workerCallInterval === 'number' && workerCallInterval > 0) {
+      this.settings.workerCallInterval = workerCallInterval;
+    } else {
+      throw new Error('Interval is incorrect');
+    }
+  }
+}
+
+// export globals
+if (typeof module !== 'undefined') {
+  module.exports = TrackChanges;
+} else {
+  window.TrackChanges = TrackChanges;
+}
